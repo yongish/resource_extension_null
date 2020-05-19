138d60682ba7c9a7eecf5a73d75a8ecb38dbf141
Zhi Yong Tan <yongish@gmail.com>
2020-05-18 21:45:03 -0700

    Initial.

---
+++ src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class HelloWorldConfiguration extends Configuration {
+}
